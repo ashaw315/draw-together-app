f8a3559653075b13c110224fcb5418225ae37b1d
Adam <ashaw315@gmail.com>
2023-04-17 11:59:24 -0400

    type fixing

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,30 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import { bucket } from '@/mongodb';
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   if (req.method === 'GET') {
-//     try {
-//       const fileId = req.query.id as string;
-//       const downloadStream = bucket.openDownloadStream(fileId);
-//       downloadStream.on('file', (file) => {
-//         if (!file.length) {
-//           throw new Error('File not found');
-//         }
-//       });
-//       res.setHeader('content-type', 'image/png');
-//       downloadStream.pipe(res);
-//     } catch (error) {
-//       console.log(error);
-//       res.status(404).json({ message: 'Image not found' });
-//     }
-//   } else {
-//     res.status(405).json({ message: 'Method not allowed' });
-//   }
-// }
-
 import { NextApiRequest, NextApiResponse } from "next";
 import { bucket } from "@/mongodb";
 
@@ -34,7 +7,7 @@ export default async function imageHandler(
 ) {
   if (req.method === 'GET') {
     try {
-      const fileIds = (await bucket.find().toArray()).map((file) => file._id.toString());
+      const fileIds = (await bucket.find().toArray()).map((file: { _id: { toString: () => string } }) => file._id.toString());
       res.status(200).json({ fileIds });
     } catch (error) {
       console.log(error);
