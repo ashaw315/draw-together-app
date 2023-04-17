ccbed75403b76569d3f4765d968da9b45be8f11b
Adam <ashaw315@gmail.com>
2023-04-17 12:22:44 -0400

    env update

--- database.ts
+++ database.ts
@@ -1,5 +1,3 @@
-// import { MongoClient } from "mongodb";
-// import * as mongoDB from "mongodb";
 const mongoDB = require('mongodb');
 
 const uri = process.env.MONGODB_URI!;
@@ -9,23 +7,3 @@ const dbName = 'mydb';
 const database = client.db(dbName);
 
 export { client, database };
-
-// const uri = process.env.MONGODB_URI!;
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
-
-// const client = new MongoClient(uri, options);
-
-// client.connect((err) => {
-//   if (err) {
-//     console.error(`MongoDB connection failed: ${err}`);
-//     return;
-//   }
-//   console.log('Connected to MongoDB');
-// });
-
-// const database = client.db('mydb');
-
-// export { client, database };
