50d61e2be619552c3954a8d2efbb2ef5fcaeaabc
Adam <ashaw315@gmail.com>
2023-04-18 14:04:47 -0400

    front server seperation

--- server/server.js
+++ server/server.js
@@ -1,35 +1,43 @@
-const express = require('express');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 let undoList = [];
 
+io.on('connection', (socket) => {
+  console.log('User Online');
+  onClear(socket);
+});
 
-io.on('connection', (socket)=> {
-    console.log('User Online');
-    onClear(socket);
-})
+function onConnection(socket) {
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  console.log('something');
 
-function onConnection(socket){
-    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-    console.log('something')
+  socket.on('addLineToUndoList', (line) => {
+    undoList.push(line);
+    console.log('LINE ADDED', line);
+    console.log(undoList.length);
+  });
+}
 
-    socket.on('addLineToUndoList', (line) => {
-      undoList.push(line);
-      console.log('LINE ADDED', line)
-      console.log(undoList.length)
-    })
-  }
+function onClear(socket) {
+  socket.on('clearCanvas', (data) => socket.broadcast.emit('clearCanvas', data));
+  console.log("cleared");
+}
 
-  function onClear(socket) {
-    socket.on('clearCanvas', (data) =>  socket.broadcast.emit('clearCanvas', data));
-      console.log("cleared");
-  }
-  
-  io.on('connection', onConnection);
+io.on('connection', onConnection);
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 4000;
-http.listen(server_port, () => {
-  console.log("Started on : "+ server_port);
-})
+app.get('/api/server', (req, res) => {
+    res.send('Socket server is running!');
+  });
+
+const server_port = process.env.PORT || 4000;
+server.listen(server_port, () => {
+  console.log("Started on : " + server_port);
+});
+
+export default app;
